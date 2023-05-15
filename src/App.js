c7f69792cd8afe50118fa5ca2133620cce058d3c
hasiburRahmanDurjoy <hasibur.rahman@vigaet.com>
2023-05-15 23:50:04 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./Components/Footer/Footer";
+import HomePageOverview from "./Components/HomePage/HomePageOverview/HomePageOverview";
+import Navigation from "./Components/Navigation/Navigation";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundColor: "#fafafa" }}>
+      <Navigation />
+      <HomePageOverview />
+      <Footer />
     </div>
   );
 }
